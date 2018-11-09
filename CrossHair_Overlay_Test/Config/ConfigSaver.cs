4861f227337116dcb64f040f3ddd1bafc8a2d50c
gmastergreatee <g77471164@gmail.com>
2018-11-09 11:52:21 +0530

    Implementation of config saver

---
+++ CrossHair_Overlay_Test/Config/ConfigSaver.cs
@@ -0,0 +1,55 @@
+﻿using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace CrossHair_Overlay_Test.Config
+{
+    public class ConfigSaver
+    {
+        public string DefaultSavePath { get; private set; } = "%APPDATA%\\CrosshairOverlay";
+
+        public ConfigSaver()
+        {
+            if (!Directory.Exists(DefaultSavePath))
+            {
+                Directory.CreateDirectory(DefaultSavePath);
+            }
+        }
+
+        public void SaveConfig(OverlayConfig overlayConfig)
+        {
+            var targetFile = DefaultSavePath + "\\" + overlayConfig.ProcessName + ".eoc";
+
+            if (File.Exists(targetFile))
+            {
+                File.Delete(targetFile);
+            }
+
+            using (var fStream = File.Create(targetFile))
+            {
+                var formatter = new BinaryFormatter();
+                formatter.Serialize(fStream, overlayConfig);
+                fStream.Close();
+            }
+        }
+
+        public OverlayConfig GetConfig(string processName)
+        {
+            var retThis = new OverlayConfig();
+            var targetFile = DefaultSavePath + "\\" + processName + ".eoc";
+            if (!File.Exists(targetFile))
+            {
+                retThis.ProcessName = processName;
+                return retThis;
+            }
+
+            using (var fStream = File.OpenRead(targetFile))
+            {
+                var formatter = new BinaryFormatter();
+                retThis = (OverlayConfig)formatter.Deserialize(fStream);
+                fStream.Close();
+            }
+
+            return retThis;
+        }
+    }
+}
---
+++ CrossHair_Overlay_Test/Config/OverlayConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Media;
+
+namespace CrossHair_Overlay_Test.Config
+{
+    public class OverlayConfig
+    {
+        public string ProcessName { get; set; }
+        public string CrosshairFileLocation { get; set; } = "";
+
+        public int OffsetX { get; set; } = 0;
+        public int OffsetY { get; set; } = 0;
+
+        public Color CrosshairColor { get; set; } = Colors.Black;
+        public int CrosshairScale { get; set; } = 2;
+        public int CrosshairTransparency { get; set; } = 100;
+    }
+}
